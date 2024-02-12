6a821a1fdfaf701d526f8c7e033582b68d01e895
Moneer <moneer.alromimah@gmail.com>
2024-02-12 13:47:55 -0700

    update

--- store/api.ts
+++ store/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+export const ADDRESS = "10.0.0.202:8000";
+
 const api = axios.create({
-  baseURL: "http://10.0.0.202:8000",
+  baseURL: `http://${ADDRESS}`,
   headers: {
     "Content-Type": "application/json",
   },
