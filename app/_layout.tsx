1680a55c29e68bdd23095da404526d1edbd8a463
Moneer <moneer.alromimah@gmail.com>
2024-02-13 14:50:14 -0700

    update with auth check

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,33 @@
-import { Stack } from "expo-router";
+import { AuthProvider } from "@/context/AuthContext";
+import useStore from "@/store/store";
+import { Stack, useRouter } from "expo-router";
+import { useEffect } from "react";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(tabs)",
+  initialRouteName: "index",
 };
 
 export default function RootLayoutNav() {
+  const authenticated = useStore((state) => state.authenticated);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!authenticated) {
+      router.push("/signIn");
+    }
+  }, [authenticated]);
+
   return (
-    <Stack>
-      <Stack.Screen name="signIn" options={{ headerShown: false }} />
-      <Stack.Screen name="signUp" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="search" options={{ headerShown: false }} />
-      <Stack.Screen name="chat" options={{ headerShown: false }} />
-    </Stack>
+    <AuthProvider>
+      <Stack screenOptions={{ contentStyle: { backgroundColor: "#fff" } }}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="search" options={{ headerShown: false }} />
+        <Stack.Screen name="chat" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="signIn" options={{ headerShown: false }} />
+        <Stack.Screen name="signUp" options={{ headerShown: false }} />
+      </Stack>
+    </AuthProvider>
   );
 }
